13fc16e69e9d52ceaa3586e5e0840e00f17569c5
Krystal <lopezkrystal10@gmail.com>
2023-04-10 13:54:29 -0700

    created server file for db

---
+++ server.js
@@ -0,0 +1,19 @@
+const { sequelize } = require("./server/models/index");
+
+const app = require("./server/app");
+
+const PORT = process.env.PORT || 8000;
+
+const init = async () => {
+  try {
+    await sequelize.sync();
+
+    app.listen(PORT, () => {
+      console.log(`Server listening at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error starting server:', error)
+  }
+};
+
+init();
