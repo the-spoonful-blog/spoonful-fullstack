b0eb76e242b14c329c2849e9fd2656d0c4785994
addrodri <addrodri@cisco.com>
2023-04-14 12:04:19 -0500

    Created seed & seedData

---
+++ server/seedData.js
@@ -0,0 +1,33 @@
+module.exports = {
+
+    users: [
+    {
+        username: "Hamburger Queen",
+        password: "Food123"
+    },
+    {
+        username: "Grill Master",
+        password: "Cow4ever",
+    },
+    {
+        username: "VeggieLuver",
+        password: "Ilyeggs"
+    }
+    ],
+
+    posts: [
+    {
+        image: "https://logo.com",
+        postBody: "This cheesy double patty hamburger is so juicy I want to eat three of them. Totally Recommend. 10/10",
+    },
+    {
+        image: "https://logo.com",
+        postBody: "Loved creating this sandwich, it was the most life changing experience of my life. 9.7/10",
+    },
+    {
+        image: "https://burgers.com",
+        postBody: "These fajita tacos are the best tacos I have ever had by far. I will definetely come back. 9/10"
+    }
+    ]
+
+}
