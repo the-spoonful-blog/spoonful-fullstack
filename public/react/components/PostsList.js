6c54d3ecf7e179c6507e801727c30c72e104e4ab
Krystal <lopezkrystal10@gmail.com>
2023-04-27 15:04:18 -0700

    cleaned up post component

--- public/react/components/PostsList.js
+++ public/react/components/PostsList.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import { Post } from './Post';
+import React from "react";
+import { Post } from "./Post";
 
-export const PostList = ({posts, isSinglePageView, setSinglePageView, postObjectTitle, setPostObjectTitle}) => {
-
+export const PostList = ({ posts, isSinglePageView, setSinglePageView, postObjectTitle, setPostObjectTitle }) => {
   return (
     <>
-      {
-        isSinglePageView
-        ? posts.filter(post => post.title === postObjectTitle).map((post1, idx) =>
-            <Post post={post1} key={idx} setPostObjectTitle={setPostObjectTitle} isSinglePageView={isSinglePageView} setSinglePageView={setSinglePageView}/>
-          )
-        : posts.map((post, idx) => <post post={post} key={idx} setPostObjectTitle={setPostObjectTitle} isSinglePageView={isSinglePageView} setSinglePageView={setSinglePageView}/>)
-      }
+      {isSinglePageView
+        ? posts
+            .filter((post) => post.title === postObjectTitle)
+            .map((post1, idx) => (
+              <Post
+                post={post1}
+                key={idx}
+                setPostObjectTitle={setPostObjectTitle}
+                isSinglePageView={isSinglePageView}
+                setSinglePageView={setSinglePageView}
+              />
+            ))
+        : posts.map((post, idx) => (
+            <Post
+              post={post}
+              key={idx}
+              setPostObjectTitle={setPostObjectTitle}
+              isSinglePageView={isSinglePageView}
+              setSinglePageView={setSinglePageView}
+            />
+          ))}
     </>
-  )
-}
+  );
+};
