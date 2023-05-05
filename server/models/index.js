761ac6d14103f0b23015e4f56af5aa45f9d1e141
dejaclarke <dejclark@cisco.com>
2023-05-05 08:54:14 -0700

    Associations

--- server/models/index.js
+++ server/models/index.js
@@ -3,6 +3,9 @@ const { sequelize } = require("../db");
 const { User } = require("./User");
 const { Post } = require("./Post");
 
+User.hasMany(Post);
+Post.belongsTo(User);
+
 module.exports = {
   sequelize,
   User,
