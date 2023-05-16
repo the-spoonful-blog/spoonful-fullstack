59cbf2c7734c73669f213c87d7f3f7ae48183110
addrodri <addrodri@cisco.com>
2023-05-16 12:17:23 -0500

    Nav bar update

--- public/react/components/Navigation.js
+++ public/react/components/Navigation.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import logo from "../../../logos/logo5.png"
 
 
 function Navigation() {
   return (
+    
     <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">The Spoonful Blog</Navbar.Brand>
+      <Container>
+          <Navbar.Brand href="/"><img src ={logo} width="30" height="30" alt= "logo"></img></Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Posts</Nav.Link>
-            <Nav.Link href="#features">Explore</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/">Posts</Nav.Link>
+            <Nav.Link href="#features">New Post</Nav.Link>
             <Nav.Link href="#features">About Us</Nav.Link>
           </Nav>
+          </Container>
       </Navbar>
   )
 }
