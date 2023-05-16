d32fc8363912515b5d868c261d0b60fa51eef168
dejaclarke <dejclark@cisco.com>
2023-05-16 12:00:56 -0700

    frontend done

--- public/react/components/Post.js
+++ public/react/components/Post.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React from "react";
+import CardGroup from "react-bootstrap/CardGroup";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export const Post = ({post, isSinglePageView, setSinglePageView, setPostObjectTitle}) => {
-
+export const Post = ({
+  post,
+  isSinglePageView,
+  setSinglePageView,
+  setPostObjectTitle,
+}) => {
   const handlePostClick = () => {
     setSinglePageView(!isSinglePageView);
     setPostObjectTitle(post.title);
-  }
+  };
 
   const handleBackButton = async () => {
     setSinglePageView(!isSinglePageView);
-  }
+  };
 
-  return <>
-
-        <div id="postContainer">
-          <h3 id="postTitle">{post.title}</h3>
-          <img id="postImage" src={post.image} alt={post.title} />
-          <p id="postBody">{post.body}</p>
-          <button onClick={!isSinglePageView ? handlePostClick : handleBackButton}>{!isSinglePageView ? `Go to post` : 'Back To List'}</button>
-        </div> 
-    
+  return (
+    <>
+      <div className="App">
+        <div>
+          <Card style={{ color: "#000" }} className="blog-Post">
+            <Card.Title className="title text-dark fs-1 fw-bold ">
+              {post.title}
+            </Card.Title>
+            <Card.Img
+              src={post.image}
+              alt={post.title}
+              className="blog-Post_Img"
+            />
+            <Card.Text>{post.body}</Card.Text>
+            <button
+              className="button"
+              onClick={!isSinglePageView ? handlePostClick : handleBackButton}
+            >
+              {!isSinglePageView ? `Go to post` : "Back To List"}
+            </button>
+          </Card>
+        </div>
+      </div>
     </>
-} 
+  );
+};
